6bd551dbd81f249f9c2ab37b1a3b5d7c919a094b
Clarkson <fredrickotabil97@gmail.com>
2022-06-02 18:12:42 +0000

    initiated react app setup && cleared all boilerplates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[#fff] h-96 w-full">
+      <h3 className="text-3xl font-addidas text-custom-green ">
+        Heya! Let's build addidas 2.0 clone
+      </h3>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        addidas: ['AdihausDIN', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'custom-green': '#00b300',
+      },
+    },
+    plugins: [],
+  },
+};
